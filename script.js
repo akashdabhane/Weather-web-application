5be2cbbf0996be3ca31de323d709217e2af5f722
Akash Dabhane <akashdabhane10@gmail.com>
2023-01-24 11:10:33 +0530

    improvement

--- script.js
+++ script.js
@@ -15,26 +15,21 @@ const getWeather = (city) => {
     fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
         .then(response => response.json())
         .then(response => {
-            console.log(response); 
+            // console.log(response);
             temparature.innerHTML = `<h3>Temparature : ${response.temp}°C</h3>`;
 
-            max_temp.innerHTML = `<li>Maximum temparature : ${response.max_temp}</li>`;
-            min_temp.innerHTML = `<li>Minimum tempareture : ${response.min_temp}</li>`;
+            max_temp.innerHTML = `<li>Maximum temparature : ${response.max_temp}°C</li>`;
+            min_temp.innerHTML = `<li>Minimum tempareture : ${response.min_temp}°C</li>`;
             sunrise.innerHTML = `<li>Sunrise : ${response.sunrise}</li>`;
             sunset.innerHTML = `<li>Sunset : ${response.sunset}</li>`;
 
-            cloud_pct.innerHTML = `<li>${response.cloud_pct}</li>`; 
-            humidity.innerHTML = `<li>Humidity : ${response.humidity}</li>`;
-            wind_degrees.innerHTML = `<li>Wind degrees : ${response.wind_degrees}</li>`;
-            wind_speed.innerHTML = `<li>Wind speed : ${response.wind_speed}</li>`;
-            // feels_like.innerHTML = `<li>${response.feels_like}</li>`;
-            // temp.innerHTML = `<li>${response.temp}</li>`;
-
+            cloud_pct.innerHTML = `<li>Cloud PCT : ${response.cloud_pct}</li>`;
+            humidity.innerHTML = `<li>Humidity : ${response.humidity}%</li>`;
+            wind_degrees.innerHTML = `<li>Wind degrees : ${response.wind_degrees}°</li>`;
+            wind_speed.innerHTML = `<li>Wind speed : ${response.wind_speed}km/h</li>`;
         })
         .catch(err => console.error(err));
-
 }
-
 
 submit.addEventListener('click', (e) => {
     e.preventDefault();
